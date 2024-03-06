61824e09c4ceccae1e2ded25d4d7628aaf4f61cb
Jesús Jiménez <jesuswebdev@gmail.com>
2024-03-06 15:40:30 -0400

    add http server for healthcheck

--- markets-observer/src/index.ts
+++ markets-observer/src/index.ts
@@ -1,8 +1,17 @@
 import { PAIRS } from '@binance-trader/shared';
 import { CANDLE_INTERVAL } from './config';
 import { Observer } from './observer';
+import http from 'http';
 
 const start = () => {
+  http
+    .createServer(function (_, res) {
+      res.statusCode = 200;
+      res.write('OK');
+      res.end();
+    })
+    .listen(8080);
+
   const observer = new Observer({
     markets: PAIRS,
     interval: CANDLE_INTERVAL,
