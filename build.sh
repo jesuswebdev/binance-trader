d3e7b167da4d4d1d94455a7e658f4c58b677684c
Jesús Jiménez <jesuswebdev@gmail.com>
2024-03-10 11:47:14 -0400

    add bash script to build all images

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+commit_hash=$1
+
+if [ -z $commit_hash ]; 
+then 
+    echo "Commit hash cannot be empty"; 
+else
+    services='account-observer candles-processor markets-observer positions-processor signals-processor trader'
+
+    for service in $services
+    do
+    echo Building image for service $service with commit hash: $commit_hash
+    docker build -t jesuswebdev/binance-trader-$service:$commit_hash -f ./$service/Dockerfile .
+    docker push jesuswebdev/binance-trader-$service:$commit_hash
+    done
+    echo All images built and pushed
+fi
+
+
