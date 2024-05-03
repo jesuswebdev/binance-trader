359f1570a204d3305012e52fb7fb2a20ed468d2a
Jesús Jiménez <jesuswebdev@gmail.com>
2024-05-03 08:49:07 -0400

    add build and update service script

---
+++ build-and-update-service.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+commit_hash="$(git rev-parse HEAD)"
+
+if [ -z $commit_hash ]; 
+then 
+    echo "Commit hash cannot be empty"; 
+else
+    services='account-observer'
+
+    for service in $services
+    do
+    echo Building image for service $service with commit hash: $commit_hash
+    docker build -t jesuswebdev/binance-trader-$service:$commit_hash -f ./$service/Dockerfile .
+    docker push jesuswebdev/binance-trader-$service:$commit_hash
+    ssh jesus@209.38.204.56 "docker pull jesuswebdev/binance-trader-$service:$commit_hash && docker service update --image jesuswebdev/binance-trader-$service:$commit_hash -d binance-trader_$service && exit"
+    done
+    echo All images built and pushed
+fi
+
+
--- build.sh
+++ build.sh
@@ -13,6 +13,8 @@ else
     echo Building image for service $service with commit hash: $commit_hash
     docker build -t jesuswebdev/binance-trader-$service:$commit_hash -f ./$service/Dockerfile .
     docker push jesuswebdev/binance-trader-$service:$commit_hash
+   # ssh jesus@209.38.204.56 "docker pull jesuswebdev/binance-trader-$service:$commit_hash && docker service update --image jesuswebdev/binance-trader-$service:$commit_hash -d binance-trader_$service && exit"
+    ssh jesus@209.38.204.56 "docker pull jesuswebdev/binance-trader-$service:$commit_hash && exit"
     done
     echo All images built and pushed
 fi
