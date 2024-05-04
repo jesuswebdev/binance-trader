cd7b8a479decb30881a565c4a0e1ee4d247c8977
Jesús Jiménez <jesuswebdev@gmail.com>
2024-05-04 17:45:14 -0400

    remvoe get tsl file

--- positions-processor/src/utils/getTrailingStopLoss.ts
+++
@@ -1,9 +0,0 @@
-import { toSymbolPrecision } from '@binance-trader/shared';
-import { POSITION_TRAILING_STOP_LOSS } from '../config';
-
-export function getTSL(price: number, symbol: string) {
-  return toSymbolPrecision(
-    +price * (1 - POSITION_TRAILING_STOP_LOSS / 100),
-    symbol,
-  );
-}
