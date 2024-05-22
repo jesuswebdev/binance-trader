5a7047a2b407208dda5530f33edbde5428089a43
Jesús Jiménez <jesuswebdev@gmail.com>
2024-05-22 10:06:43 -0400

    update build scripts to replace image tag on execution

--- build-and-update-service.sh
+++ build-and-update-service.sh
@@ -6,14 +6,17 @@ if [ -z $commit_hash ];
 then 
     echo "Commit hash cannot be empty"; 
 else
-    services='account-observer'
+    services='trader'
 
     for service in $services
     do
     echo Building image for service $service with commit hash: $commit_hash
     docker build -t jesuswebdev/binance-trader-$service:$commit_hash -f ./$service/Dockerfile .
+    sed -i -e "s/.*image: jesuswebdev\/binance-trader-$service:.*/    image: jesuswebdev\/binance-trader-$service:$commit_hash/g" stack.yml
     docker push jesuswebdev/binance-trader-$service:$commit_hash
-    ssh jesus@209.38.204.56 "docker pull jesuswebdev/binance-trader-$service:$commit_hash && docker service update --image jesuswebdev/binance-trader-$service:$commit_hash -d binance-trader_$service && exit"
+    ssh jesus@209.38.204.56 "docker pull jesuswebdev/binance-trader-$service:$commit_hash && exit"
+    # ssh jesus@209.38.204.56 "docker pull jesuswebdev/binance-trader-$service:$commit_hash && docker service update --image jesuswebdev/binance-trader-$service:$commit_hash -d trader-api_$service && exit"
+    # sed?
     done
     echo All images built and pushed
 fi
--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ else
     do
     echo Building image for service $service with commit hash: $commit_hash
     docker build -t jesuswebdev/binance-trader-$service:$commit_hash -f ./$service/Dockerfile .
+    sed -i -e "s/.*image: jesuswebdev\/binance-trader-$service:.*/    image: jesuswebdev\/binance-trader-$service:$commit_hash/g" stack.yml
     docker push jesuswebdev/binance-trader-$service:$commit_hash
    # ssh jesus@209.38.204.56 "docker pull jesuswebdev/binance-trader-$service:$commit_hash && docker service update --image jesuswebdev/binance-trader-$service:$commit_hash -d binance-trader_$service && exit"
     ssh jesus@209.38.204.56 "docker pull jesuswebdev/binance-trader-$service:$commit_hash && exit"
